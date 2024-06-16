4b4cae9d83a1a7f01b7cd8e0b9ccecede7c837e0
John <wheeler6123@gmail.com>
2024-06-16 15:42:16 -0600

    updated solution for problem 2620 Counter

--- 2620counter.js
+++ 2620counter.js
@@ -33,10 +33,21 @@ At most 1000 calls to counter() will be made
  * @param {number} n
  * @return {Function} counter
  */
+
+// P: two inputs, one an integer for the initial value of the counter which could be a whole number between -1000 and 1000, the second an array containing various number of strings that say "call", representing the number of times the function should be called. This array will be between 0 and 1000 in length
+// R: the return should be an integer value. The first time the function is called, it should return the input integer, and for each 'call' in the input array, it should return the previous value plus 1.
+// E: n = 10, ['call', 'call', 'call'] => 10, 11, 12
+//    n = -2, ['call', 'call', 'call', 'call', 'call'] => -2, -1, 0, 1, 2
+// P: Question - The constraints indicate an empty array can be passed for calls, but we are not told what to return if the array is empty. What should be returned in this edge case?
+// First step is to initialize a counter = n;
+// return a function that returns count++;
+
 var createCounter = function(n) {
-    return function(){
-        return n++
-    }
+    let count = n;
+
+    return function() {
+        return count++;
+    };
 };
 
 /** 
