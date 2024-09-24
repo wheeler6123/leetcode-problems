43714f2a5177c1e4463023437e801bf1c7a7f0e5
John <wheeler6123@gmail.com>
2024-09-24 16:45:11 -0400

    add solution for 1071

--- 1071greatestCommonDivisorOfStrings.js
+++ 1071greatestCommonDivisorOfStrings.js
@@ -22,3 +22,32 @@
 
 // 1 <= str1.length, str2.length <= 1000
 // str1 and str2 consist of English uppercase letters.
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+ // P: two inputs, both strings of uppercase letters with length between 1 and 1000
+ // R: return a string representing the largest string, x, that divides both input strings
+ //    if strings have no common divisor, return an empty string
+ // E: 'ABCABC', 'ABC' => 'ABC'
+ //    'ABABAB', 'ABAB' => 'AB'
+ //    'LEET', 'CODE' => ''
+ // P: verify if inputs are divisible by checking that str1 + str2 is the same as str2 + str1, if not, return ''
+ //    if inputs are divisible, find the greatest common denominator for the two string lengths
+ //    get a substring that runs from 0 to the greatest common denominator length and return 
+
+ function gcd(a,b){
+    if (b === 0){
+        return a;
+    }
+    return gcd(b, a%b);
+}
+
+var gcdOfStrings = function(str1, str2){
+    if(str1 + str2 !== str2 + str1) return '';
+    let gcdNumeric = gcd(str1.length, str2.length);
+    return str1.substring(0, gcdNumeric);
+}
